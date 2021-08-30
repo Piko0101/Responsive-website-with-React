917e0153d800525dabda8c3bff0055061ce0aa3a
perizat <psabitbek@magnumtech.kz>
2021-08-30 16:49:01 +0600

    add path to pages

--- personalProjects/learn-react/src/App.js
+++ personalProjects/learn-react/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
@@ -9,9 +13,13 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact  component={Home}/>
+          <Route path="/products" exact  component={Products}/>
+          <Route path="/services" exact  component={Services}/>
+          <Route path="/sign-up" exact  component={SignUp}/>
         </Switch>
       </Router>
+    
     </>
   );
 }
